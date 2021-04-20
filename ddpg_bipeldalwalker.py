0eb44448ece88c4628fa01a85230f0a1950736ba
YufengJin <jyf931118@gmail.com>
2021-04-20 11:54:35 +0200

    add bipeldalwalker wandb files

---
+++ ddpg_bipeldalwalker.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+from stable_baselines3 import SAC, TD3
+from stable_baselines3.common import results_plotter
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.results_plotter import load_results, ts2xy, plot_results
+from stable_baselines3.common.noise import NormalActionNoise
+from stable_baselines3.common.callbacks import BaseCallback
+import wandb
+import os
+import gym
+
+wandb.init(name='SAC', project="BipedalWalker")
+
+class WandbCallback(BaseCallback):
+    """
+    Custom callback for plotting additional values in tensorboard.
+    """
+
+    def __init__(self, check_freq: int, log_dir: str, verbose=1):
+        super(WandbCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, 'best_model')
+        self.best_mean_reward = -np.inf
+
+
+    def _init_callback(self) -> None:
+        # Create folder if needed
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self) -> bool:
+        if self.n_calls % self.check_freq == 0:
+
+          # Retrieve training reward
+          x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+          if len(x) > 0:
+              # Mean training reward over the last 100 episodes
+              mean_reward = np.mean(y[-100:])
+              if self.verbose > 0:
+                wandb.log({'Average Reward': mean_reward})
+
+              # New best model, you could save the agent here
+              if mean_reward > self.best_mean_reward:
+                  self.best_mean_reward = mean_reward
+                  # Example for saving best model
+                  if self.verbose > 0:
+                    print("Saving new best model to {}".format(self.save_path))
+                  self.model.save(self.save_path)
+
+        return True
+
+# Create log dir
+log_dir = "./BipedalWalker-v3"
+os.makedirs(log_dir, exist_ok=True)
+
+
+# Create and wrap the environment
+env = gym.make('BipedalWalker-v3')
+env = Monitor(env, log_dir)
+# n_actions = env.action_space.shape[-1]
+# action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+# model = TD3('MlpPolicy', env, action_noise=action_noise, verbose=1)
+model = SAC('MlpPolicy', env, verbose=1)
+callback = WandbCallback(check_freq=100, log_dir=log_dir)
+model.learn(50000, callback=callback)
